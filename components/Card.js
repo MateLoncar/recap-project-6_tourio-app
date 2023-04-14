edb1bd36b72b7f6ccfe69ef4d4b4bdc11f1977ac
Mate Loncar <mate.loncar1995@gmail.com>
2023-04-14 12:04:09 +0200

    work work

--- components/Card.js
+++ components/Card.js
@@ -42,7 +42,7 @@ const ScreenReaderOnly = styled.span`
   border-width: 0;
 `;
 
-export default function Card({ name, image, location, id }) {
+export default function Card({ name, image, location, _id }) {
   return (
     <Article>
       <Figure>
@@ -59,7 +59,7 @@ export default function Card({ name, image, location, id }) {
         <figcaption>{name}</figcaption>
       </Figure>
       <p>Location: {location}</p>
-      <Link href={`places/${id}`} passHref legacyBehavior>
+      <Link href={`places/${_id}`} passHref legacyBehavior>
         <Anchor>
           <ScreenReaderOnly>More Info</ScreenReaderOnly>
         </Anchor>
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,25 @@
-import { places } from '../../../lib/db';
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(req, res) {
+  await dbConnect();
+
+  if (req.method === "GET") {
+    try {
+      const places = await Place.find();
+      res.status(200).json(places);
+    } catch (error) {
+      res.status(500).json({ error: "Error getting places from database" });
+    }
+  } else if (req.method === "POST") {
+    const { name, description } = req.body;
+    try {
+      const newPlace = await Place.create({ name, description });
+      res.status(201).json(newPlace);
+    } catch (error) {
+      res.status(500).json({ error: "Error adding place to database" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
 }
